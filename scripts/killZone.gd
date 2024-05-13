695d57887223d545a6e4778c6a2ecb3986d0b2ee
nikobalek <nikobalek1381@gmail.com>
2024-05-13 09:24:12 +0330

    added slowmotion while dies and deactivates collision

--- scripts/killZone.gd
+++ scripts/killZone.gd
@@ -3,8 +3,11 @@ extends Area2D
 
 
 func _on_body_entered(body):
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
 	print("you died")
 	timer.start()
 
 func _on_timer_timeout():
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
