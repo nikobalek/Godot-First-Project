70ddc092d3dc2fdd76c736a89ab78fbf1fc69aa3
nikobalek <nikobalek1381@gmail.com>
2024-05-15 09:53:00 +0330

    killzone sfx added

--- scripts/killZone.gd
+++ scripts/killZone.gd
@@ -1,11 +1,13 @@
 extends Area2D
 @onready var timer = $Timer
+@onready var sfx = $sfx
 
 
 func _on_body_entered(body):
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
 	print("you died")
+	sfx.play()
 	timer.start()
 
 func _on_timer_timeout():
