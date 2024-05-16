e73cf16708e30d370cd84c2cf91130ecdb886206
nikobalek <nikobalek1381@gmail.com>
2024-05-16 17:25:07 +0330

    fixed jump and double jump

--- scripts/charachter.gd
+++ scripts/charachter.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 @onready var jump_sfx = $jump_sfx
 
 const SPEED = 150.0
-const JUMP_VELOCITY = -250.0
+const JUMP_VELOCITY = -280.0
 
 var jumps = 0
 
@@ -18,10 +18,10 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump"):
-		
-		if is_on_floor():
+	if is_on_floor():
 			jumps = 0
+			
+	if Input.is_action_just_pressed("jump"):
 		if jumps < 2:
 			jump_sfx.play()
 			velocity.y = JUMP_VELOCITY
