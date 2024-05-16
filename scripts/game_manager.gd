8b4634bdc2e061087df5b0c6fd9d9405877f3af8
nikobalek <nikobalek1381@gmail.com>
2024-05-16 18:03:20 +0330

    fixed coin count GUI

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,13 +1,16 @@
 extends Node
 
 
-
 @onready var label = $"../CanvasLayer/Label"
 
 var score = 0
+var ltext = "0"
 
 func add_point():
 	score += 1
-	print(score)
-	label.text = str(score)
+	if score > 9:
+		ltext = "   " + str(score)
+	else:
+		ltext = str(score)
+	label.text = ltext
 
