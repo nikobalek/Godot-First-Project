992f244436855a031f90e863f3a8e9b45c463ba8
nikobalek <nikobalek1381@gmail.com>
2024-05-25 04:04:35 +0330

    pause added

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -5,6 +5,15 @@ extends Node
 
 var score = 0
 var ltext = "0"
+var pause = false
+
+func _process(delta):
+	if Input.is_action_just_pressed("escape") and pause == false:
+		Engine.time_scale = 0
+		pause = true
+	elif Input.is_action_just_pressed("escape") and pause == true:
+		Engine.time_scale = 1
+		pause = false
 
 func add_point():
 	score += 1
