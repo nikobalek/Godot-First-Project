13fec307f9e59f10b69302293c0f6b64f396261f
nikobalek <nikobalek1381@gmail.com>
2024-05-25 04:08:47 +0330

    better pause

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,6 +2,7 @@ extends Node
 
 
 @onready var label = $"../CanvasLayer/Label"
+@onready var character_body_2d = $"../CharacterBody2D"
 
 var score = 0
 var ltext = "0"
@@ -10,9 +11,13 @@ var pause = false
 func _process(delta):
 	if Input.is_action_just_pressed("escape") and pause == false:
 		Engine.time_scale = 0
+		character_body_2d.canJump = false
+		character_body_2d.canMove = false
 		pause = true
 	elif Input.is_action_just_pressed("escape") and pause == true:
 		Engine.time_scale = 1
+		character_body_2d.canJump = true
+		character_body_2d.canMove = true
 		pause = false
 
 func add_point():
